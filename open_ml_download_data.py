51319bea6e61e9cc55e769ccffea2e575486e31e
Andy Stevko <andy.stevko+github@brainome.ai>
2022-02-25 19:15:49 -0800

    refactored /Dropbox out

--- open_ml_download_data.py
+++ open_ml_download_data.py
@@ -1,5 +1,5 @@
 """
-this script is used to download TRAIN_URL into cached TRAIN_FILE
+this script is used to download TRAIN_URL into cached data/TRAIN_FILE
 """
 
 import csv
@@ -7,19 +7,20 @@ import urllib.request
 from pathlib import Path
 
 SUITE_FILE = './test-suites/open_ml_select.tsv'
+DATA_DIR = Path("./data")
 
 with open(SUITE_FILE, newline='') as scenario:
     reader = csv.DictReader(scenario, delimiter='\t')
     for idx, params in enumerate(reader):
         train_filename = params['TRAIN_FILE']
         train_url = params['TRAIN_URL']
-        train_file = Path(train_filename)
+        train_file = DATA_DIR / train_filename
         if not train_file.exists():
             uri_filename = urllib.request.\
                 urlopen(urllib.request.Request(train_url, method='HEAD')).\
                 info().get_filename()
             url_file = train_file.parents[0] / uri_filename
-            print(f'{train_filename} does not exist, downloading {url_file}')
+            print(f'{str(train_file)} does not exist, downloading {train_url}')
             urllib.request.urlretrieve(train_url, url_file)
         else:
             print(f"found {train_filename}")
